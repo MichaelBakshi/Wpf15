9fd21c2d5a0a3864fbeb39bc67b86d8332ee5f12
Michael Bakshi <michael.bakshi83@gmail.com>
2021-04-24 18:41:47 +0300

    Add project files.

---
+++ Wpf15/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Order> orders = new List<Order>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            orders.Add(new Order() { Id = 1, Name = "John", Surname = "Doe", Age = 20, Email = "JohnDoe@Mail.com", MembershipOwner = false, SumOfOrder = 153, OrderStatus = MyEnum.New });
+
+            dgorders.ItemsSource = orders;
+
+        }
+        public enum MyEnum { None=0, New=1, Processing=2, Shipped=3, Received=4 }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Wpf15/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Wpf15.MainWindow;
+
+namespace Wpf15
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public bool MembershipOwner { get; set; }
+        public double SumOfOrder { get; set; }
+        public MyEnum OrderStatus { get; set; }
+    }
+}
